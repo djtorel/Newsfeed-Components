526dcc95b1f8c5a8e741e7196bcd55d9f7870528
Dominic Torruellas <dom@domtorr.com>
2019-04-15 22:20:48 -0700

    finished article events

--- Article/Article.js
+++ Article/Article.js
@@ -1,22 +1,35 @@
+const bySelectorAll = (selector, cb = false) => {
+  const nodeList = document.querySelectorAll(selector);
+  return cb ? cb(nodeList) : nodeList;
+};
+
+const byElementSelector = (selector, element, cb = false) => {
+  const selectedElement = element.querySelector(selector);
+  return cb ? cb(selectedElement) : selectedElement;
+};
+
 // Because classes are not hoisted you will need to start your code at the
 // bottom of the page.  Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class.
-    this.expandButton;
+    this.expandButton = byElementSelector('.expandButton', this.domElement);
     // Using your expandButton reference, update the text on your expandButton
     // to say "expand"
-
+    this.expandButton.innerText = 'expand';
     // Set a click handler on the expandButton reference, calling the
     // expandArticle method.
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide
     // the article.
+    console.log(this.domElement);
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -31,4 +44,7 @@ class Article {
 
 */
 
-let articles;
+let articles = bySelectorAll('.article');
+articles.forEach(element => {
+  new Article(element);
+});
